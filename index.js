b3fbf0edd9b61ab7e3fc1840e847f35dcad698fb
Visal20497 <visal20497@gmail.com>
2024-05-10 02:58:34 +0530

    new

---
+++ index.js
@@ -0,0 +1,18 @@
+// for providing the shadow
+let url="https://shadows.brumm.af/"
+
+// for creating the border radius
+ let url2="https://9elements.github.io/fancy-border-radius/#47.47.67.97--."
+
+
+
+ // for creating the wave in our html
+ let url3="https://getwaves.io/"
+
+
+
+
+ 
+
+
+
